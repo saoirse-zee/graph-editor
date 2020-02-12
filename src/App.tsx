c8982e6e41f233c81fb22c6ace4879c22140fb0b
Emma Smith <saoirse-zee@users.noreply.github.com>
2020-02-11 19:40:33 -0500

    Draw row line between nodes

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,25 @@ import { createList, createNode } from "./linked-list";
 import Container from "./components/Container";
 import "./App.css";
 
-
 // Temporary test data.
 const n1 = createNode();
 const n2 = createNode();
 const n3 = createNode();
 n1.setData([100, 130]);
 n2.setData([110, 135]);
-n3.setData([115, 140]);
+n3.setData([215, 170]);
 const list = createList(n1);
 list.append(n2);
 list.append(n3);
+
+const rowToLineData = (rowArray: any[]) => {
+  return rowArray
+    .map(node => {
+      const point = node.getData();
+      return `${point[0]},${point[1]}`;
+    })
+    .join(" ");
+};
 
 const App = () => {
   const [inputX, setInputX] = useState();
@@ -21,6 +29,8 @@ const App = () => {
   const [row, setRow] = useState(list);
 
   const rowNodes = row.toArray();
+  const lineData = rowToLineData(rowNodes);
+  console.log(lineData);
 
   // Handlers
   const handleAddClick = (e: any) => {
@@ -56,6 +66,7 @@ const App = () => {
         <button>Add</button>
       </form>
       <Container>
+        <polyline points={lineData} fill="none" stroke="white" strokeWidth={3} opacity={0.3} />
         {rowNodes.map((n, i) => (
           <circle
             key={i}
