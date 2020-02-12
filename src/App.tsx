c8ada26f85889e50f200cf3980b7f482ef98603a
Emma Smith <saoirse-zee@users.noreply.github.com>
2020-02-12 17:36:55 -0500

    wip hotkeys

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { HotKeys } from "react-hotkeys";
 import { createList, createNode } from "./linked-list";
-import Container from "./components/Container";
+
 import "./App.css";
+import { Editor } from "./components/Editor";
 
 let id = 0;
 
@@ -108,62 +109,19 @@ const App = () => {
 
   return (
     <HotKeys
-      keyMap={{ APPEND: "a" }}
+      keyMap={{ APPEND: "a", RIGHT: "right", L: "l" }}
       handlers={{
-        APPEND: append
+        APPEND: append,
+        RIGHT: () => handleNudge(Direction.RIGHT),
+        L: () => handleNudge(Direction.RIGHT),
       }}
     >
       <div className="App">
-        <div style={{ display: "flex", fontFamily: "monospace" }}>
-          <div style={{ marginRight: 50 }}>
-            <p>Selected: {selected}</p>
-            <p>x: {selectedPoint && selectedPoint[0]}</p>
-            <p>y: {selectedPoint && selectedPoint[1]}</p>
-          </div>
-          <form onSubmit={handleUpdateClick}>
-            <div>
-              <label>x</label>
-              <input onChange={handleXChange} />
-            </div>
-            <div>
-              <label>y</label>
-              <input onChange={handleYChange} />
-            </div>
-            <button>Update</button>
-          </form>
-        </div>
-        <button onClick={handlePrev}>Prev</button>
-        <button onClick={handleNext}>Next</button>
-        <button onClick={() => handleNudge(Direction.RIGHT)}>
-          Nudge right
-        </button>
-        <button onClick={() => handleNudge(Direction.LEFT)}>Nudge left</button>
-        <button onClick={() => handleNudge(Direction.UP)}>Nudge up</button>
-        <button onClick={() => handleNudge(Direction.DOWN)}>Nudge down</button>
-        <Container handleClick={handleMapClick}>
-          <g
-            onClick={e => {
-              console.log(e);
-            }}
-          >
-            <polyline
-              points={lineData}
-              fill="none"
-              stroke="white"
-              strokeWidth={3}
-              opacity={0.3}
-            />
-            {rowNodes.map(n => (
-              <circle
-                key={n.getId()}
-                cx={n.getData()[0]}
-                cy={n.getData()[1]}
-                r={n.getId() === selected ? 7 : 5}
-                fill={"white"}
-              />
-            ))}
-          </g>
-        </Container>
+        {selected}
+        <Editor handlers={{
+          handleNext,
+          handlePrev,
+        }} />
       </div>
     </HotKeys>
   );
